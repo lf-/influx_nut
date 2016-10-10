e8ec290978d7ec3e08a1536062fd7e09d00202e9
lf- <lf-@users.noreply.github.com>
2016-10-10 16:02:58 -0600

    Document config more better

--- influx_nut.py
+++ influx_nut.py
@@ -12,10 +12,16 @@ import requests
 __doc__ = """A small daemon and library for sending NUT statistics to influxdb"""
 
 DEFAULT_CONFIG = {
+    # interval between updates, in seconds
     'interval': 20,
+    # ip/hostname of NUT server
     'nut_host': '127.0.0.1',
+    # NUT port
     'nut_port': 3493,
+    # UPS name on that NUT server
     'nut_ups': 'ups1',
+
+    # variables from NUT to send to influxdb, see above
     # example in json:
     # "nut_vars": {
     #   "ups.realpower.nominal": {
@@ -24,9 +30,17 @@ DEFAULT_CONFIG = {
     #   }
     # }
     'nut_vars': {},
-    'influx_host': 'http://127.0.0.1:8086',
+
+    # proto://host:port of influxdb. Please don't add a trailing slash.
+    'influx_host': 'http://127.0.0.1:8086', 
+
+    # database on the influx server
     'influx_db': 'systems',
+
+    # tags to add to influxdb measurements
     'influx_tags': {},
+
+    # credentials for influxdb
     # example in json:
     # "influx_creds": ["user", "sekrit"]
     'influx_creds': None
